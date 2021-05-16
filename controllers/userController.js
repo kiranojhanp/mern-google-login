32a182efdab96c6da14f7bdcadd789cd37a5ff84
Kiran ojjha <kiranojha1226@gmail.com>
2021-05-16 10:23:39 +0545

    send email using nodemailer and gmail

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,14 @@
 const asyncHandler = require("express-async-handler");
+const axios = require("axios");
+
+// use googleapis to exchange refresh token for access token dynamically and use nodemailer to send email
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+
+// for validating accessToken
 const { OAuth2Client } = require("google-auth-library");
 const User = require("../models/UserModel");
 const generateToken = require("../util/generateToken");
-const axios = require("axios");
 
 const client = new OAuth2Client(process.env.CLIENT_ID);
 
@@ -11,7 +17,6 @@ const client = new OAuth2Client(process.env.CLIENT_ID);
 // @access Public
 const facebookLogin = asyncHandler(async (req, res) => {
   const { userID, accessToken } = req.body;
-  // console.log(userID, accessToken);
 
   let urlGraphFacebook = `https://graph.facebook.com/${userID}?fields=name,email,picture&access_token=${accessToken}`;
   const { data } = await axios.get(urlGraphFacebook);
@@ -47,8 +52,6 @@ const facebookLogin = asyncHandler(async (req, res) => {
       res.status(400).json({ error: "Could not create!" });
     }
   }
-
-  // console.log(name, email, picture);
 });
 
 // @desc  Authenticate the users using google
@@ -96,4 +99,43 @@ const googleLogin = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { facebookLogin, googleLogin };
+// @desc  Send password reset email to user
+// @route  POST /api/auth/reset
+// @access Public
+const resetPassword = asyncHandler(async (req, res) => {
+  const { emailTo } = req.body;
+  const oAuth2Client = new google.auth.OAuth2(
+    process.env.MAILER_CLIENT_ID,
+    process.env.MAILER_CLIENT_SECRET,
+    process.env.MAILER_REDIRECT_URI
+  );
+  oAuth2Client.setCredentials({
+    refresh_token: process.env.GMAIL_REFRESH_TOKEN,
+  });
+
+  const accessToken = await oAuth2Client.getAccessToken();
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: "kiranojha1226@gmail.com",
+      clientId: process.env.MAILER_CLIENT_ID,
+      clientSecret: process.env.MAILER_CLIENT_SECRET,
+      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+      accessToken: accessToken,
+    },
+  });
+
+  const mailOptions = {
+    from: "Timro Buda😇 <kiranojha1226@gmail.com>",
+    to: emailTo,
+    subject: "Eh sexy timi kati ramri",
+    text: "I love you budi",
+  };
+
+  const result = await transport.sendMail(mailOptions);
+
+  res.json({ message: "Email is sent", result });
+});
+
+module.exports = { facebookLogin, googleLogin, resetPassword };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { googleLogin, facebookLogin } = require("../controllers/userController");
+const { googleLogin, facebookLogin, resetPassword } = require("../controllers/userController");
 
 router.route("/google").post(googleLogin);
 router.route("/facebook").post(facebookLogin);
+
+router.route("/reset").post(resetPassword);
 
 module.exports = router;
